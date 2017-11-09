e50eb4ce7b89879f78080b2a99a7b7c725a3f4dd
RafaelleMaru <villegasrafaelle@gmail.com>
2017-11-09 13:02:02 +0800

    First Upload
    First project upload with bootstrap template.

---
+++ Development/MeditechStaffing.Portal/MeditechStaffing.Portal/Controllers/PoliciesAndProceduresController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MeditechStaffing.Portal.Controllers
+{
+    public class PoliciesAndProceduresController : Controller
+    {
+        // GET: PoliciesAndProcedures
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult CommunityStaff()
+        {
+            return View();
+        }
+
+        public ActionResult BestPractices()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ Development/MeditechStaffing.Portal/MeditechStaffing.Portal/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MeditechStaffing.Portal.MvcApplication" Language="C#" %>
